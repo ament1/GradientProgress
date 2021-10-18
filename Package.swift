2c4c5f99a77f47c379a8cfd8688520877cbdd8d1
ament1 <92379188+ament1@users.noreply.github.com>
2021-10-18 12:41:59 +0200

    Added SPM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,25 @@
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
-    name: "GradientProgress"
+    name: "GradientProgress",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "GradientProgress",
+            targets: ["GradientProgress"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "GradientProgress",
+            dependencies: [],
+            path: "Sources"
+        ),
+    ]
 )
